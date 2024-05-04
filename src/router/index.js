9089c32879e189812a2acc78924af1de834fa472
bernat ferrandiz <bernatferrandiz@gmail.com>
2024-05-04 12:29:05 +0200

    Fix ruta inici

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,10 @@ const routes = [
     {
         path: "/receptes",
         component: () => import("@/views/ReceptesPage.vue")
+    },
+    {
+        path: "/inici",
+        component: () => import("@/views/iniciPage.vue")
     }
 ];
 
